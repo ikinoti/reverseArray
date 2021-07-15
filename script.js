8ed5022e758ac5c59c4f38d19f8bafc234c90f1a
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-07-15 10:25:59 +0300

    Add a js code to reverse an array

---
+++ script.js
@@ -0,0 +1,8 @@
+let A = ['this', 'is', 'good']
+
+let reverseArr = function(A) {
+  let resultsArr = A.reverse();
+  document.getElementById('revs').textContent = resultsArr;
+  /*  return resultsArr; */
+}
+// reverseArr(A);
